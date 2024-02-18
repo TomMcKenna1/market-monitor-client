be312e4b5b9b9ea0f369ec8af9f3f1ebf4cbe536
Tom McKenna <tom2mckenna@gmail.com>
2024-02-18 10:47:12 +0000

    feat: added location access screen

--- screens/AddDeviceScreen.tsx
+++ screens/AddDeviceScreen.tsx
@@ -1,13 +1,72 @@
-import { ActivityIndicator, SafeAreaView, View } from "react-native";
+import {useEffect} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 const AddDeviceScreen = props => {
-    return (
-        <SafeAreaView style={{flex: 1}}>
-            <View style={{justifyContent: 'center', flex: 1}}>
-                <ActivityIndicator size="large" color="#0FA3B1"/>
+  useEffect(() => {}, []);
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{justifyContent: 'center', flex: 1}}>
+        <View style={{justifyContent: 'center', flex: 1}}>
+          <Text style={{textAlign: 'center', fontSize: 30}}>
+            Location access
+          </Text>
+          <Text style={{padding: 20, textAlign: 'center', fontSize: 17}}>
+            Location access is needed to detect nearby Bluetooth and Wi-Fi
+            signals, and to communicate with your Market Monitor device.
+          </Text>
+          <Text style={{padding: 20, textAlign: 'center', fontSize: 17}}>
+            Tap next to enable precise location.
+          </Text>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+          <Icon color="#0FA3B1" name="map-location-dot" size={150} />
+        </View>
+        <View style={{padding: 20, alignItems: 'flex-end'}}>
+          <TouchableOpacity activeOpacity={0.75} style={{padding: 15}}>
+            <View
+              style={{
+                backgroundColor: '#0FA3B1',
+                paddingLeft: 10,
+                paddingRight: 10,
+                borderRadius: 20,
+                shadowColor: 'black',
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.23,
+                shadowRadius: 2.62,
+                elevation: 4,
+              }}>
+              <Text style={{padding: 20, textAlign: 'center', fontSize: 17, color: "white"}}>
+                Next
+              </Text>
             </View>
-        </SafeAreaView>
-    )
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{display: 'none', justifyContent: 'center', flex: 1}}>
+        <Text
+          style={{
+            position: 'absolute',
+            top: 150,
+            width: '100%',
+            textAlign: 'center',
+            fontSize: 30,
+          }}>
+          Looking for devices
+        </Text>
+        <ActivityIndicator size="large" color="#0FA3B1" style={{padding: 10}} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
-export default AddDeviceScreen
+export default AddDeviceScreen;
